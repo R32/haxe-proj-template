bd00168ee34df2b071157473cc884c11f3679d37
235 <hotup@qq.com>
2016-08-05 14:11:58 +0800

    benchmark-fib fix

--- benchmark-fib/src/Run.hx
+++ benchmark-fib/src/Run.hx
@@ -3,7 +3,6 @@ package;
 // haxe -cp src -main Run --interp
 class Run{
 	static function main(){
-		// run.bat
 		var cmd = [
 			"neko" => ["bin/ben.n"],	// neko
 			"node" => ["bin/ben.js"],	// nodejs
@@ -11,16 +10,14 @@ class Run{
 			"java" => ["-jar", "bin/java/ben.jar"],	// hxjava(java)
 			"bin/cs/bin/Ben.exe" => [],	// hxcs(c#)
 			"bin/cpp/Ben.exe" => [],	// hxcpp(cpp)
-			                            // no flash
 		];
 
 		for (k in cmd.keys()) {
 			var c = is_windows ? k.split("/").join("\\") : k;
 			var t0 = haxe.Timer.stamp();
 			var p = new sys.io.Process(c, cmd.get(k));
-			var t1 = haxe.Timer.stamp() - t0;
-
 			if (p.exitCode() == 0) {
+				var t1 = haxe.Timer.stamp() - t0;
 				Sys.print(p.stdout.readAll().toString());
 				Sys.print('*** from start to finish. [TIME: $t1] ***\n\n');
 			} else {

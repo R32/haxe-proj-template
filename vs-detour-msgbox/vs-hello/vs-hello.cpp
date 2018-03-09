a8b74842f41b46cebb842306d740e1c44b5e69d9
r32 <56782055@qq.com>
2018-03-09 17:03:36 +0800

    use DetourFindFunction

--- vs-detour-msgbox/vs-hello/vs-hello.cpp
+++ vs-detour-msgbox/vs-hello/vs-hello.cpp
@@ -3,22 +3,12 @@
 
 #include "stdafx.h"
 
-//保存函数原型
-static int (WINAPI *OldMesssageBoxW)(
-	_In_opt_ HWND hWnd,
-	_In_opt_ LPCWSTR lpText,
-	_In_opt_ LPCWSTR lpCaption,
-	_In_ UINT uType) = MessageBoxW;
+typedef int (WINAPI *PFMessageBoxW)(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType);
 
-//改写函数
-static int WINAPI NewMessageBoxW(
-	_In_opt_ HWND hWnd,
-	_In_opt_ LPCWSTR lpText,
-	_In_opt_ LPCWSTR lpCaption,
-	_In_ UINT uType)
-{
-	return OldMesssageBoxW(NULL, L"new MessageBox", L"Please", MB_OK);
+static PFMessageBoxW OrgMessageBoxW = NULL;
 
+int WINAPI MyMessageBoxW(HWND hWnd, LPCWSTR lpText, LPCWSTR lpCaption, UINT uType) {
+	return ((PFMessageBoxW)OrgMessageBoxW)(hWnd, L"Hook This!", L"My hook", uType);
 }
 
 void StartHook()
@@ -27,8 +17,10 @@ void StartHook()
 	DetourTransactionBegin();
 	//更新线程信息
 	DetourUpdateThread(GetCurrentThread());
+	//DetourFindFunction
+	OrgMessageBoxW = (PFMessageBoxW)DetourFindFunction("User32.dll", "MessageBoxW");
 	//将拦截的函数附加到原函数的地址上
-	DetourAttach(&(PVOID&)OldMesssageBoxW, NewMessageBoxW);
+	DetourAttach(&(PVOID&)OrgMessageBoxW, MyMessageBoxW);
 	//结束事务
 	DetourTransactionCommit();
 }
@@ -41,7 +33,7 @@ void EndHook()
 	//更新线程信息
 	DetourUpdateThread(GetCurrentThread());
 	//将拦截的函数从原函数的地址上解除
-	DetourDetach(&(PVOID&)OldMesssageBoxW, NewMessageBoxW);
+	DetourDetach(&(PVOID&)OrgMessageBoxW, MyMessageBoxW);
 	//结束事务
 	DetourTransactionCommit();
 }
@@ -53,7 +45,6 @@ int _tmain(int argc, _TCHAR* argv[]) {
 	//应原样输出
 	EndHook();
 	MessageBoxW(NULL, L"old MessageBox", L"Please", MB_OK);
-	system("pause");
 	return 0;
 }
 

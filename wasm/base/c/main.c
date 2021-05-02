64b30a2c08b953046b268626326fe691422cef85
r32 <56782055@qq.com>
2021-05-02 09:12:51 +0800

    updated wasm/base

--- wasm/base/c/main.c
+++ wasm/base/c/main.c
@@ -1,4 +1,39 @@
-// export from: wasm-ld --export=square
-int square(int num) {
-	return num * num;
+// copy from em_macros.h
+#define EM_IMPORT(NAME) __attribute__((import_module("env"), import_name(#NAME)))
+
+// if there is no "--allow-undefined" in "wasm-ld",
+// you need to explicitly add "EM_IMPORT(NAME)" instead of "extern"
+// when you passed a pointer to JS, you could find it in webassembly.Memory
+EM_IMPORT(log) void log(char* x, int a, int b, int c, int d, int e, int f);
+
+// The built-in values, uses &__xxxx to access it. e.g: "&__heap_base"
+// then the stack size is: "&__heap_base - &__data_end";
+extern int __data_end;           // the end offset of the data section
+extern int __global_base;        // __data_base ???
+extern int __heap_base;          // the start offset of the heap
+extern int __memory_base;        // ths start offset of the whole memeory???
+extern int __dso_handle;
+extern int __table_base;
+
+extern void __wasm_call_ctors(); // no idea what's this
+
+// export by "wasm-ld --export=square" in Makefile
+int square(int n) {
+	return n * n;
+}
+
+#define EM_EXPORT(name) __attribute__((used, export_name(#name)))
+// export by macro
+EM_EXPORT(test) char* arbitrary_name() {
+	char* s1 = "string literal";
+	char* s2 = "others";
+	log(s1,
+		(int)&__data_end,
+		(int)&__global_base,
+		(int)&__heap_base,
+		(int)&__memory_base,
+		(int)&__table_base,
+		(int)&__dso_handle
+	);
+	return s2;
 }
--- wasm/base/src/Main.hx
+++ wasm/base/src/Main.hx
@@ -14,8 +14,20 @@ class Main {
 		if (!WebAssembly.validate(buffer))
 			return;
 		var mod = new Module(buffer);
-		var lib = new Instance(mod, {});
-		js.Lib.global.lib = lib.exports;
-		trace(lib.exports.square(101));
+		//var mem = new Memory({initial: 10}); // 64K * 10
+		var lib = new Instance(mod, {
+			env : {
+				log: function(a, b, c, d, e, f, g) {
+					js.Browser.console.log(a,
+						"data_end: " + b, "global_base: " + c , "heap_base: " + d,
+						"memory_base:" + e, "table_base:" + f, "dso_handle:" + g
+					);
+				},
+			}
+		});
+		var clib = lib.exports;
+		js.Lib.global.lib = clib;
+		trace(clib.square(101));
+		trace(clib.test());
 	}
 }
